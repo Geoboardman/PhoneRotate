1628e2db1d9a4e0851a5746e509795839c700425
Richard <saville.rich@gmail.com>
2017-03-04 12:23:04 -0800

    Improved version of control for phone

--- Assets/Scripts/PhoneControl.cs
+++ Assets/Scripts/PhoneControl.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class PhoneControl : MonoBehaviour {
 
     int speed = 50;
-
+    Quaternion newRotation = Quaternion.identity;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	//This function takes in a Vector3, which is just an array of 3 float values
+    //It creates a new rotation angle for the object based on the 3 values
+    //And slowly rotates the object from its current rotation, to the new one
+    //Based on the speed variable at the top.
+
+    public void RotatePhone(Vector3 angles)
+    {
+        newRotation = Quaternion.Euler(angles);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, Time.deltaTime * speed);
+    }
+
 	// Update is called once per frame
 	void Update () {
+        RotatePhone(new Vector3(0, 90, 0));
+
+        //The code below will rotate an object base on key presses
+        /*
         if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(Vector3.right * speed * Time.deltaTime);
@@ -37,5 +52,6 @@ public class PhoneControl : MonoBehaviour {
         {
             transform.Rotate(Vector3.back * speed * Time.deltaTime);
         }
+        */
     }
 }
